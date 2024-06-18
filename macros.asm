5b4f89ea278e68ffac516e0ba0c88916501f3a20
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-18 22:07:36 +0100

    added vram addressing mode and wip print semantic

--- macros.asm
+++ macros.asm
@@ -14,10 +14,30 @@ VDP_DATA            EQU         $C00000
 VDP_CTRL            EQU         $C00004
 
 ;--------------------------------------------------------
+;       THIS VRAM MACRO COROUTINE WILL LOOK TO 
+;   DETERMINE IF THERE IS A VALID OFFSET MASK WITHIN
+;       A MAX VALUE RELATED TO THE REGISTERS
+;
+;   THIS WILL SET A VALID VRAM ADDRESS TO THE VDP CTRL PORT
+;                       USING DMA
+;
+;           THIS ROUTINE IS SIMILAR TO SONIC 1
+;--------------------------------------------------------
+
+VRAM:               MACRO LOCATION, CONTROL
+                    IF("controlport"=="")
+                    MOVE.L  #($40000000+(((LOCATION)&$3FFF)<<16)+(((LOCATION)&VDP_DATA)>>14)),(VDP_CTRL).L
+                    ELSE
+                    MOVE.L  #($40000000+(((LOCATION)&$3FFF)<<16)+(((LOCATION)&VDP_DATA)>>14)),CONTROL
+                    ENDIF
+                    ENDM
+
+;--------------------------------------------------------
 ;       ADDRESSABLE HEADER ARGUMENTS FOR RETURNING
 ;       BITWISE VALUES FOR ERROR HANDLING 
 ;--------------------------------------------------------
 
+ERROR_HANDLER_FONT:     EQU     $7C0    
 
 ADDRESS_ERROR_FLAG      EQU     $01
 SR_USP_ERROR_FLAG       EQU     $02
@@ -34,22 +54,31 @@ ERROR_TYPE              DS.B    1
 ;               GLOBAL PRINT SEMANTIC
 ;       WORKS ON THE BASIS OF INCREMENTING THE BYTE
 ;       OF EACH CHARACTER TO PRINT TO SCREEN
+;
+;       THIS IS RETOOLED TO FORCEABLY INJECT
+;       THE ERROR MESSAGE INTO THE VDP'S DMA
 ;--------------------------------------------------------
 
-PRINT_STDERR:           MACRO       STRING, FLAGS
-    JSR                 ERROR_HANDLER(PC)
-    DC.B                \STRING, 0
-    even
-    JMP                 ERROR_HANDLER_EXT(PC)
-    ENDM
+PRINT_STDERR:
+    LEA             (VDP_DATA).L, A6
 
-ERROR_HANDLER:          
-        MOVE            #$2700, SR                      ;; DISABLE INTERRUPTS BASED ON CURRENT SR OFFSET
-        MOVEM.L         D0-D6, -(SP)
+PRINT_STDERR_VALUE:
 
-ERROR_HANDLER_EXT:      
-    MOVEM.L             D0-D6, -(SP)
-    MOVE.W              #0, -(SP)
+
+;--------------------------------------------------------
+;   SHOW THE CURRENT ALLOCATED ERROR IN THE BUFFER
+;   THIS IS SO THAT THE ERROR HANDLER IS ABLE TO
+;   PROPERLY ALLOCATE THE ERROR INTO THE STACK
+;--------------------------------------------------------
+
+ERROR_ALLOC_IN_BUFFER:
+    ADDQ.W              #2, SP
+    MOVE.L              (SP)+,(ERROR_SP_BUFFER).W
+    ADDQ.W              #2, SP
+    MOVEM.L             D0-D7, (EXCE_REG_BUFFER).W
+    BSR.W               PRINT_STDERR
+    MOVE.K              2(SP), D0
+    BSR
 
 @MAIN_LOOP:
     LEA                 VDP_CTRL, A5
