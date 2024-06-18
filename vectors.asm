2bca78a47c42858a3bc99015c603a1e6194d3efc
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-18 18:27:04 +0100

    updated access level for directive

--- vectors.asm
+++ vectors.asm
@@ -32,7 +32,7 @@ VECTORS:
 
 ENTRY_POINT:
     MOVE            #$2700, SR                  ;; DISABLE INTERRUPTS BASED ON CURRENT SR OFFSET
-    LEA             @SETUP_VALUES(PC), A0       ;; LOAD CURRENT EXECUTION FROM PC TO A0
+    LEA             SETUP_VALUES(PC), A0       ;; LOAD CURRENT EXECUTION FROM PC TO A0
     MOVEM.L         (A0)+, A2-A5                ;; INIT ADDRESS REGISTERS
 
     TST.W           $A1000C-$A11100(A3)         ;; IS PORT C INIT?
@@ -44,43 +44,52 @@ ENTRY_POINT:
 HW_CHECK:
     MOVEQ           #$F, D7
     AND.B           $A10001-$A11100(A3), D7     ;; ACCESS HARDWARE REV
-    BEQ.S           @HW_DONE                    ;; BRANCH ASSUMING THAT THE RESULT IS EQUAL TO THE AND COMPARISON (SEE: main.asm)
-    MOVE.L          #'SEGA' $A14000             ;; CHECK FOR SEGA CHAR IN ORDER TO INIT VDP                    
+    BEQ.S           HW_DONE                    ;; BRANCH ASSUMING THAT THE RESULT IS EQUAL TO THE AND COMPARISON (SEE: main.asm)
+    MOVE.L          #'SEGA', $A14000             ;; CHECK FOR SEGA CHAR IN ORDER TO INIT VDP
+
+;--------------------------------------------------------
+;   THIS COROUTINE WILL LOOK TO MAKE SURE THAT THE Z80
+;   BUS IS ABLE TO COMMUNICATE WITH THE VDP IN ORDER TO INIT
+;--------------------------------------------------------
+
+HW_DONE:              
+    MOVE.W      D1, (A3)
+    MOVE.W      D1, (A4)
     
-BUS_ERROR:          ASM_EXTERN
+BUS_ERROR:          
     PRINT_STDERR    "BUS ERROR", EXCE_DEFAULT | ADDRESS_ERROR_FLAG
 
-ADDRESS_ERROR:      ASM_EXTERN
+ADDRESS_ERROR:      
     PRINT_STDERR    "ADDRESS ERROR", EXCE_DEFAULT | ADDRESS_ERROR_FLAG
 
-ILLEGAL_INSTR:      ASM_EXTERN
+ILLEGAL_INSTR:     
     PRINT_STDERR    "ILLEGAL INSTRUCTION", EXCE_DEFAULT
 
-ZERO_DIV:           ASM_EXTERN
+ZERO_DIV:           
     PRINT_STDERR    "ZERO DIVISION", EXCE_DEFAULT
 
-CHK_INSTR:          ASM_EXTERN
+CHK_INSTR:          
     PRINT_STDERR    "CHECK INSTRUCTION", EXCE_DEFAULT
 
-TRAPV_INSTR:        ASM_EXTERN
+TRAPV_INSTR:        
     PRINT_STDERR    "TRAP_V INSTRUCTION", EXCE_DEFAULT
 
-PRIV_VIOL:          ASM_EXTERN
+PRIV_VIOL:          
     PRINT_STDERR    "PRIVILEEGE VIOLATION", EXCE_DEFAULT
 
-TRACE:              ASM_EXTERN
+TRACE:              
     PRINT_STDERR    "TRACE", EXCE_DEFAULT
 
-LINE_1010:          ASM_EXTERN
+LINE_1010:          
     PRINT_STDERR    "LINE_1010 EMULATOR", EXCE_DEFAULT
 
-LINE_1111:          ASM_EXTERN
+LINE_1111:          
     PRINT_STDERR    "LINE_1111 EMULATOR", EXCE_DEFAULT
 
-ERROR_EXCEPT:       ASM_EXTERN
+ERROR_EXCEPT:       
     PRINT_STDERR    "ERROR EXCEPTION", EXCE_DEFAULT
 
-IDLE_INT:           ASM_EXTERN
+IDLE_INT:           
     RTE
 
 ;--------------------------------------------------------
@@ -89,7 +98,7 @@ IDLE_INT:           ASM_EXTERN
 ;       TRAP WILL LOOK TO STOP CPU EXECUTION
 ;--------------------------------------------------------
 
-ERROR_TRAP:         ASM_EXTERN
+ERROR_TRAP:         
     NOP
     BRA.S   ERROR_TRAP
 
@@ -98,7 +107,7 @@ ERROR_TRAP:         ASM_EXTERN
 ;   WITH THE VDP, Z80, INITALISATION OF PORTS, TMSS, ETC
 ;--------------------------------------------------------
 
-@SETUP_VALUES:      ASM_EXTERN
+SETUP_VALUES:      
 
     DC.L            $C00004                 ; VDP CTRL
     DC.L            $A11100                 ; Z80 BUS REQ
