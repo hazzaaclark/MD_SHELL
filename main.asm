780065f9d1679439e5e752d4807c446cfc3e8b87
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-19 22:21:33 +0100

    main program rewrite

--- main.asm
+++ main.asm
@@ -5,6 +5,7 @@
 ;--------------------------------------------------------
 
             INCLUDE "macros.asm"
+            INCLUDE "vectors.asm"
 
 ;--------------------------------------------------------
 ;   THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY
@@ -36,11 +37,11 @@ VECTORS:
 ;--------------------------------------------------------
 
 ROM_START:
-    DC.B        'SEGA MEGA DRIVE '                          ;; CONSOLE NAME 
-    DC.B        '(C)HARRY CLARK '                           ;; AUTHOR NAME 
-    DC.B        '    SEGA MEGA DRIVE SHELL                      '            ;; DOMESTIC NAME
-    DC.B        '    SEGA MEGA DRIVE SHELL                      '            ;; INTERNATION NAME (NOT USED)
-    DC.B        '   GM-00000000-00'                                                ;; CODE
+    DC.B        'SEGA MEGA DRIVE '                                               ;; CONSOLE NAME 
+    DC.B        '(C)HARRY CLARK '                                               ;; AUTHOR NAME 
+    DC.B        '    SEGA MEGA DRIVE SHELL                      '               ;; DOMESTIC NAME
+    DC.B        '    SEGA MEGA DRIVE SHELL                      '               ;; INTERNATIONAL NAME (NOT USED)
+    DC.B        '   GM-00000000-00'                                             ;; CODE
     DC.W        0x0000                                                          ;; SP
     DC.B        'J              '                                               ;; IO SUPPORT 
     DC.L        ROM_START                                                       ;; ROM START
@@ -50,7 +51,7 @@ ROM_START:
     DC.L        $20202020                                                       ;; SRAM START
     DC.L        $20202020                                                       ;; SRAM END
     DC.B        "                                               "
-    DC.B        "JUE            "                                                ;; REGION
+    DC.B        "JUE            "                                               ;; REGION
 
 
 ;--------------------------------------------------------
@@ -62,46 +63,8 @@ ROM_START:
 ;   ADDRESS REGISTERS TO READ THE HARDWARE CHECKS TO RAM
 ;--------------------------------------------------------
 
+
 ENTRY_POINT:
-    MOVE            #$2700, SR                  ;; DISABLE INTERRUPTS BASED ON CURRENT SR OFFSET
-    LEA             SETUP_VALUES(PC), A0       ;; LOAD CURRENT EXECUTION FROM PC TO A0
-    MOVEM.L         (A0)+, A2-A5                ;; INIT ADDRESS REGISTERS
-
-    TST.W           $A1000C-$A11100(A3)         ;; IS PORT C INIT?
-
-    MOVEM.W         (A0)+, D0-D6                ;; INIT DATA REGISTERS
-
-    BRA             HW_CHECK                    ;; BRANCH TO CHECK FOR HARDWARE DEFINES
-
-PROG_ENTRY:
-    MOVE        #$2700, SR              ;; DISABLE INTERRUPTS IN STACK REGISTER BASED ON OFFSET
-    LEA         SETUP_VALUES(PC), A0
-    MOVEM.L     (A0)+, A2-A5
-
-    TST.W       $A1000C-$A11100(A3)
-    BNE.S       @INIT_DONE
-    MOVEM.W     (A0)+, D0-D6  
-
-    MOVEQ       #$F, D7
-    AND.B       $A10001-$A11100(A3), D7
-    BEQ         HW_DONE
-    MOVE.L      $100.W, $A10001-$A11100(A3)
-
-
-@INIT_DONE:
-    BRA       PROG_START
-
-PROG_START:
-    MOVEA.L     0.W, SP
-    MOVEQ       #$40, D0
-    MOVE.B      D0, $A10009
-    MOVE.B      D0, $A1000B
-
-    MOVE        #$2700, SR
-
-
-
-    INCLUDE "vectors.asm"
 
 
 ROM_END:
